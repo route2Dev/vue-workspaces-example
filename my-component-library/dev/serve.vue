b19dd7a0ab2c2c58511f401dbb416ad26d118e7f
DAVE BAUMANN <dave.baumann@route2solutions.com>
2022-02-20 21:51:22 -0600

    initial commit

---
+++ my-component-library/dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MyComponentLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  MyComponentLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <my-component-library-sample />
+  </div>
+</template>
---
+++ my-component-library/src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as MyComponentLibrarySample } from './my-component-library-sample.vue';
